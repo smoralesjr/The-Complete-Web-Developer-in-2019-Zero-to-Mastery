038f9714f625a8f7ffa34a55df368f377e68dfec
Samuel Morales Jr <smoralesjr@gmail.com>
2019-08-27 09:54:20 -0400

    Solution to Keyless Car exercise

--- JavaScript/Make A Keyless Car/car.js
+++ JavaScript/Make A Keyless Car/car.js
@@ -11,3 +11,15 @@
 
 // IF they say they are over 18, respond with:
 // "Powering On. Enjoy the ride!"
+
+var age;
+
+age = Number(prompt("Please enter your age: "));
+
+if (age === 18) {
+  alert("Congratulations on your first year of driving. Enjoy the ride!");
+} else if (age < 18) {
+  alert("Sorry, you are too young to drive this car. Powering off.");
+} else if (age > 18) {
+  alert("Powering On. Enjoy the ride!");
+}
