29bcc84f5af8a99a1e82d1414af5c990c64725cf
Samuel Morales Jr <smoralesjr@gmail.com>
2019-09-01 11:31:56 -0400

    solution to Shopping List exercise

--- DOM Manipulation/Shopping List/shopping.js
+++ DOM Manipulation/Shopping List/shopping.js
@@ -1,5 +1,28 @@
-var button = document.getElementsByTagName("button")[0];
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
+var userinput = document.getElementById("userinput");
 
+function checkLength(){
+    return input.value.length;
+}
+
+function createListElement() {
+    var li = document.createElement("li");
+        li.appendChild(document.createTextNode(userinput.value));
+        ul.appendChild(li);
+        userinput.value = "";
+}
 button.addEventListener("click", function(){
-    console.log("CLICK!!!");
-});
+    if (checkLength() > 0){
+        createListElement();
+    } 
+    
+})
+
+input.addEventListener("keypress", function(event) {
+    if (checkLength() > 0 && event.keyCode === 13) {
+        createListElement();
+    } 
+    
+})
