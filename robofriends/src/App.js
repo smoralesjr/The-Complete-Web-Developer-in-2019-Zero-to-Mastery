8ee21b918887a96d01dcca54f90cbb2692eeaf1d
Samuel Morales Jr <smoralesjr@gmail.com>
2019-09-27 14:23:29 -0400

    refactoring, adding a searchbox

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, {Component} from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import { robots } from './robots';
 
-const App = () => {
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state =  {
+      robots: robots,
+      searchfield: '' 
+    }
+  }
+ render () {
   return (
-    <div>
+    <div className='tc'>
       <h1>RoboFriends</h1>
       <SearchBox />
-      <CardList robots={robots} />
+      <CardList robots={this.state.robots} />
     </div>
   );
 }
-
+}
 export default App;
